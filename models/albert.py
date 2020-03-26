a770f603f5465e881f5e9286955cc244c6af94a7
zingp <605156398@qq.com>
2020-03-26 14:51:23 +0800

    评论数据预处理

--- models/albert.py
+++ models/albert.py
@@ -22,12 +22,12 @@ class Config(object):
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)                         # 类别数
         self.num_epochs = 5                                             # epoch数
-        self.batch_size = 128                                           # mini-batch大小
+        self.batch_size = 64                                            # mini-batch大小
         self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-5                                       # 学习率
         self.albert_path = './albert_pretrain'
         self.tokenizer = BertTokenizer.from_pretrained(self.albert_path)
-        self.hidden_size = 768
+        self.hidden_size = 1024
 
 
 class Model(nn.Module):
@@ -35,14 +35,14 @@ class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
         self.albert = AlbertModel.from_pretrained(config.albert_path)
-        for param in self.bert.parameters():
+        for param in self.albert.parameters():
             param.requires_grad = True
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
 
     def forward(self, x):
         context = x[0]  # 输入的句子
         mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
-        _, pooled = self.albert(context, attention_mask=mask, output_all_encoded_layers=False)
+        _, pooled = self.albert(context, attention_mask=mask)
         """
         参数:
         	config: bert模型的参数
